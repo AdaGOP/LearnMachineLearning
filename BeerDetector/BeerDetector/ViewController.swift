9fd581d64ffe98c9a2530c6e1b17aad994e7c237
zein rezky chandra <zeinrezky@gmail.com>
2020-05-12 09:28:27 +0700

    - Adding documentation in line

--- BeerDetector/BeerDetector/ViewController.swift
+++ BeerDetector/BeerDetector/ViewController.swift
@@ -21,11 +21,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var selectedImage: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
     
-    lazy var classificationRequest: VNCoreMLRequest = {
+    lazy var analyseRequest: VNCoreMLRequest = { // Create property request to VNCoreML based on ML Model
         do {
-            let model = try VNCoreMLModel(for: BeerClassifier().model)
+            let model = try VNCoreMLModel(for: BeerClassifier().model) // Inisiate the ML Model
             let request = VNCoreMLRequest(model: model, completionHandler: {   [weak self] request, error in
-                self?.processClassifications(for: request, error: error)
+                self?.processToAnalyse(for: request, error: error) // Evaluate the result and update the UI
             })
             request.imageCropAndScaleOption = .centerCrop
             return request
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pickAction(_ sender: UIButton) {
-        openLibrary()
+        openLibrary() // Func to call image selection from Library
     }
     
 }
@@ -72,7 +72,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 //            let newImage = imageTaken.resizeImage(30.0, opaque: false)
             picker.dismiss(animated: true) {
                 self.selectedImage.image = imageTaken
-                self.createClassificationsRequest(image: imageTaken)
+                self.createAnalysisRequest(image: imageTaken) // After image selected, create request analysis based on the image
             }
         }
     }
@@ -82,37 +82,37 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 }
 
 extension ViewController {
-    func createClassificationsRequest(image: UIImage) {
+    func createAnalysisRequest(image: UIImage) {
         resultLabel.text = "Analyzing..."
-        let orientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue))
+        let orientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue)) // Convert UIImage to CIImage, CIImage are the format compatible with core image filters
         guard let ciImage = CIImage(image: image)
         else {
-          fatalError("Unable to create \(CIImage.self) from \(image).")
+          fatalError("Unable to create \(CIImage.self) from \(image).") // handle if conversion goes fail
         }
-        DispatchQueue.global(qos: .userInitiated).async {
-           let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation!)
+        DispatchQueue.global(qos: .userInitiated).async { // Create under background thread with qos priority, due to heavy task from image processing
+           let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation!) // create instance for the request handler
            do {
-            try handler.perform([self.classificationRequest])
+            try handler.perform([self.analyseRequest]) // perform request based on the handler and analyse it and update the result to UI under main thread
            } catch {
             print("Failed to perform \n\(error.localizedDescription)")
            }
         }
     }
     
-    func processClassifications(for request: VNRequest, error: Error?) {
+    func processToAnalyse(for request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
             guard let results = request.results
             else {
               self.resultLabel.text = "Unable to analyze image.\n\(error!.localizedDescription)"
               return
             }
-            let classifications = results as! [VNClassificationObservation]
+            let classifications = results as! [VNClassificationObservation] // create instance for observation returned by VNCoreMLRequests that using a model that is a classifier. A classifier produces an array of classifications which are labels and confidence scores.
             if classifications.isEmpty {
                 self.resultLabel.text = "Nothing recognized."
             } else {
-                let topClassifications = classifications.prefix(2)
+                let topClassifications = classifications.prefix(2) // only get 2 important information, which are the confidence value and identifier value
                 let descriptions = topClassifications.map { classification in
-                    return String(format: "(%.2f) %@", classification.confidence, classification.identifier)
+                    return String(format: "(%.2f) %@", classification.confidence, classification.identifier) // convert the key value from the result and construct into a readable string
                 }
                 self.resultLabel.text = descriptions.joined(separator: " | ")
             }
